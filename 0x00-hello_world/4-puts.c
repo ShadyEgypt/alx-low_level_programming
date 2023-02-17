3090c7e2f2da2107e5fe651d58449d6a8dcc64ec
ShadyEgypt <shadyrafat60@gmail.com>
2023-02-17 04:07:49 +0200

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
-* main - this function prints a line and returns 0
-* return: 0
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
