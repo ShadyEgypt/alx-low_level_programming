00ce265fc92210e777cecb58935eeba4a2c4344c
ShadyEgypt <shadyrafat60@gmail.com>
2023-02-21 21:13:18 +0200

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet in lowercase
+ * Description: This is 2nd task in the project
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i='a';i<='z';i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Print alphabet in lowercase
+ * followed by a new line.
+ * Description: This is 2nd task in the project
+ * Return: 0
+ */
+int main(void)
+{
+        print_alphabet();
+        return (0);
+}
+
