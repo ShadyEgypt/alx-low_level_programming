2e1e54bd7cecc46b144e99905bd88f73abf6a4aa
ShadyEgypt <shadyrafat60@gmail.com>
2023-02-22 00:38:12 +0200

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+/**
+ * _isalpha - checks if an entry is a letter or not
+ * Description: Get an entry from user and prints
+ * 1 in case it's a letter and 0 in case it's not
+ * @c: a variable to be entered by user
+ * @x: a variable to get the result of isalpha func
+ * Return: 0 or 1
+ */
+
+int _isalpha(char c)
+{
+	int x = isalpha(c);
+	if (x > 0)
+		return (1);
+	else
+		return (0);
+}
