9740963286430ef719ca3aa8c454e4647964069e
ShadyEgypt <shadyrafat60@gmail.com>
2023-02-22 00:50:15 +0200

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,13 +4,13 @@
  * Description: Get an entry from user and prints
  * 1 in case it's a letter and 0 in case it's not
  * @c: a variable to be entered by user
- * @x: a variable to get the result of isalpha
+ * @x - a variable to get the result of isalpha
  * Return: 0 or 1
  */
 
 int _isalpha(char c)
 {
-/* @x: a var to hold the result of isalpha */
+
 	int x = isalpha(c);
 
 	if (x > 0)
