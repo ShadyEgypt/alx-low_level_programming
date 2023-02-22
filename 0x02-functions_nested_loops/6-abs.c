b11e475254e00b7b7d851b83ceb33efd5a2a86b0
ShadyEgypt <shadyrafat60@gmail.com>
2023-02-22 03:13:06 +0200

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlibo.h>
+/**
+ * _abs - get the absoluite of a number
+ * Description: if a number is negative
+ * it makes it positive
+ * Return: either 0 or a positive number
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
