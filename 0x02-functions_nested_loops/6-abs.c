1284386b933f1181c30cf29e2e3f827016324c9a
ShadyEgypt <shadyrafat60@gmail.com>
2023-02-22 03:15:55 +0200

    task6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,6 +3,7 @@
  * _abs - get the absoluite of a number
  * Description: if a number is negative
  * it makes it positive
+ * @n: a number to be entered by user
  * Return: either 0 or a positive number
  */
 
