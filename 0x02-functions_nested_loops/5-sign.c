6cbfe99ea3000ce6a65e49071cb79b49a45b19e9
ShadyEgypt <shadyrafat60@gmail.com>
2023-02-22 02:32:38 +0200

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/* print_sign - prints the sign of a digit
+ * Description:  Get an entry from user and prints
+ * whether it's positive, negative or zero
+ * @n: type int integer
+ * Return: 0 or 1 or -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return(0);
+	}
+}
