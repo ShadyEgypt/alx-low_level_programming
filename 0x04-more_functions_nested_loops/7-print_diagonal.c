7e3b414f268a69cfdfd9a8ac895afed4f89d61d7
ShadyEgypt <shadyrafat60@gmail.com>
2023-02-24 02:27:12 +0200

    task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: number of times the '\' char is printed
+ * @c - a number representing the spaces before the slash
+ * @i - an iterator to put 'c' spaces before the slash
+ * Description: Can only use _putchar to print
+ */
+void print_diagonal(int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		c++;
+		n--;
+	}
+	if (c < 1)
+		_putchar('\n');
+}
