aba0f91d77d197556a6b5ce957f9c1f8dd613421
ShadyEgypt <shadyrafat60@gmail.com>
2023-02-28 02:54:01 +0200

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _puts - print a string to stdout
+ * @str: char array string type
+ * Description: using standard library, print string
+ */
+
+void _puts(char *str)
+{
+    puts(*str)
+}
