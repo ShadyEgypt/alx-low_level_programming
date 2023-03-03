0998c37db028695719e85f6b868e45c5e5569702
ShadyEgypt <shadyrafat60@gmail.com>
2023-03-03 04:54:27 +0200

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * leet - encode a string into 1337
+ * *leet - encode a string into 1337
  * @s: string
  * @letters - Array of letters needed to be encoded
  * @num - Array of coressponding code numbers for letters
@@ -12,15 +12,16 @@
 
 char *leet(char *s)
 {
-	char letters[] = "AEOTL"
-	char num[] = "43071"
+	char letters[] = "AEOTLaeotl";
+	char num[] = "4307143071";
 	int i;
 	int j;
-	for (j = 0; j != '\0'' j++)
+
+	for (i = 0; letters[i] != '\0'; i++)
 	{
-		for (i = 0; letters[i] != '\0'; i++)
+		for (j = 0; s[j] != '\0'; j++)
 		{
-			if ( letters[i] == s[j] || letters[i] == (s[j]+=32) )
+			if (s[j] == letters[i])
 				s[j] = num[i];
 		}
 	}
