b6a2eb1da82406a401ea83d433c7d9f3585117de
ShadyEgypt <shadyrafat60@gmail.com>
2023-03-09 03:25:06 +0200

    task3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,7 +8,13 @@
 
 int factorial(int n)
 {
-	if (n == 0)
-		return (1);
-	return (n * factorial(n - 1));
+    if (n < 0)
+    {
+        n = -1 * n
+        return ((1 ^ -n) * factorial(n))
+    } else {
+        if (n == 0)
+		    return (1);
+	    return (n * factorial(n - 1));
+    }
 }
