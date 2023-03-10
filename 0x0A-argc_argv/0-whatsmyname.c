95b5d1bff14f0668abbdaeb07699ad375ce05e6a
ShadyEgypt <shadyrafat60@gmail.com>
2023-03-11 00:30:16 +0200

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: 0
  */
 
- int main (int argc, char **argv)
- {
-    (void) argc;
-    printf("%s\n", *argv);
-    return (0);
- }
+int main (int argc, char **argv)
+{
+(void) argc;
+printf("%s\n", *argv);
+return (0);
+}
