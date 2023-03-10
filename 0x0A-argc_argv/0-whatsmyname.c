7ce4bab80bb7c08e96b2e4beeaa3dfba6ddea3e9
ShadyEgypt <shadyrafat60@gmail.com>
2023-03-10 20:12:45 +0200

    first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the program followed by a new line
+ * @argc: argument count
+ * @argv: arguments passed through command line
+ * Return: 0
+ */
+
+ int main (int argc, char **argv)
+ {
+    (void) argc;
+    printf("%s\n", *argv);
+    return (0);
+ }
